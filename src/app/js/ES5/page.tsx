d364f36627c884811056b5caade84fceeb9178bb
noorali <jasmine@jasmines-MacBook-Air.local>
2023-05-05 10:51:02 +0330

    es5 part 2

--- src/app/js/ES5/page.tsx
+++ src/app/js/ES5/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 
@@ -5,10 +6,25 @@ export default function Index() {
   const string1 = "this is my string in this file";
   const string2 = "     hello world      ";
   let string3 = "";
-  const myobject = { name: "john", age: 24 };
   const array1 = ["banana", "orange", "apple", "mango"];
   const array2 = [23, 45, 65, 43, 78];
-  array2.forEach((e) => (string3 = string3 + e));
+  const object1 = { name: "john", age: 24 };
+  const object2 = '{"name" : "john" , "age" : "24" }';
+  array2.forEach((e) => {
+    return (string3 = string3 + e);
+  });
+  const objectGS = {
+    firstName: "john",
+    lastName: "doe",
+    age: 0,
+    get fullName() {
+      return this.firstName + " " + this.lastName;
+    },
+    set setAge(value: number) {
+      this.age = value;
+    },
+  };
+  objectGS.age = 24;
 
   return (
     <>
@@ -28,7 +44,7 @@ export default function Index() {
       <pre>"hello" + "world"</pre>
       <hr />
       <p> obj = name:"john", age:24 </p>
-      <p>obj.name : {myobject.name}</p>
+      <p>obj.name : {object1.name}</p>
       <hr />
       <p>trim(): this method removes white spaces from both sides of string </p>
       <p>
@@ -79,12 +95,97 @@ export default function Index() {
         left to right but array.reduceRight() iterates ove the lements from
         right to left
       </p>
-      <p>
-        [23,45,65,43,78].reduceRight((total,value) ={">"} total +
-        value){" "}
-      </p>
+      <p>[23,45,65,43,78].reduceRight((total,value) ={">"} total + value) </p>
       <p>{array2.reduceRight((t, v) => t + v)}</p>
       <hr />
+      <p>
+        array.every() : checks if all array values pass a test. return boolean
+      </p>
+      <p>
+        {" "}
+        [23,45,65,43,78].every((value,index,array)={">"} value {">"} 60) :
+        allover is{" "}
+        {array2.every((i) => {
+          return i < 60;
+        })}
+      </p>
+      <hr />
+      <p>
+        array.some() : checks if some aray values pass a test, returns a boolean
+      </p>
+      <p>
+        [23,45,65,43,78].some((value)={">"} value {">"} 60) : allover is{" "}
+        {array2.some((i) => {
+          return i > 60;
+        })}
+      </p>
+      <hr />
+      <p>
+        array.indexOf() : search an array for an element value and return it's
+        position
+      </p>
+      <p>
+        ["banana", "orange", "apple", "mango"].indexOf("mango") :{" "}
+        {array1.indexOf("mango")}
+      </p>
+      <hr />
+      <p>
+        array.lastIndexOf() : is the same of array.indexOf(),but searches from
+        the end of the array{" "}
+      </p>
+      <p>
+        ["banana", "orange", "apple", "mango"].lastIndexOf("mango") :{" "}
+        {array1.lastIndexOf("mango")}
+      </p>
+      <hr />
+      <p>JSON.parse() : converts the text into javascript object</p>
+      <p>
+        JSON.parse({object2}).name : {JSON.parse(object2).name}
+      </p>
+      <hr />
+      <p>JSON.stringfy() : converts object into string</p>
+      <p>
+        JSON.stringfy({"{"} name: "john", age: 24 {"}"}) :{" "}
+        {JSON.stringify(object1)}
+      </p>
+      <hr />
+      <p>
+        Data.now() : returns a number of miliseconds since zero date (jan
+        1.1970)
+      </p>
+      <p>Date.now() : {Date.now()}</p>
+      <hr />
+      <p>
+        date.toISOString() : converts a date object to a string, using the ISO
+        standard format
+      </p>
+      <p> (new Date()).toISOString() : {new Date().toISOString()}</p>
+      <hr />
+      <p>
+        data.toJSON : convers a date object into a string, formatted as JSON
+        date
+      </p>
+      <p>(new Date()).toJSON() :{new Date().toJSON()}</p>
+      <hr />
+      
+      <hr />
+      <p>Property Getters and Setters</p>
+      <p>
+        es5 lets you define object methods with a syntax that looks like getting
+        or setting a property.
+      </p>
+      <p>
+        const objectGS = {"{"}
+        firstName: "john", lastName: "doe", age: 0, get fullName() {"{"}
+        return this.firstName + " " + this.lastName;
+        {"}"}, set setAge(value: number) {"{"}
+        this.age = value;
+        {"}"},{"}"}; objectGS.age = 24{" "}
+      </p>
+      <p>object.fullName : {objectGS.fullName}</p>
+      <p>object.age : {objectGS.age}</p>
+      <hr />
+
     </>
   );
 }
