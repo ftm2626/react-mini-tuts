d5ad13b2e4ce08bfa0285eeda27af90280192dd4
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2024-04-15 11:40:11 +0330

    finished testing, FINALLY!

--- src/app/jest/components/8.mocks/2.handlers.ts
+++ src/app/jest/components/8.mocks/2.handlers.ts
@@ -1,9 +1,6 @@
 import { HttpResponse, http } from "msw";
 export const handlers = [
   http.get("https://jsonplaceholder.typicode.com/users", (info) => {
-    return HttpResponse.json([
-        {name:"jasi"},
-        {name:"fatemeh"}
-    ]);
+    return HttpResponse.json([{ name: "jasi" }, { name: "fatemeh" }]);
   }),
 ];
--- src/app/jest/components/8.users.test.tsx
+++ src/app/jest/components/8.users.test.tsx
@@ -1,1 +1,27 @@
-// 46
+import { HttpResponse, http } from "msw";
+import { render, screen } from "../test-utils";
+import { server } from "./8.mocks/1.server";
+import { Users } from "./8.user";
+
+describe("users", () => {
+  test("render correctly", () => {
+    render(<Users />);
+    const textElement = screen.getByText("Users");
+    expect(textElement).toBeInTheDocument();
+  });
+
+//   test("renders a list of users",async ()=>{
+//     render(<Users />);
+//     const users = await screen.findAllByRole("listitem")
+//     expect(users).toHaveLength(3)
+//   })
+
+// test("render error" ,()=>{
+//     server.use(http.get("https://jsonplaceholder.typicode.com/users",()=>{
+//         return HttpResponse.error()
+//     }));
+// })
+
+// this needs more setting for moking. just read the msw docs.
+
+});
